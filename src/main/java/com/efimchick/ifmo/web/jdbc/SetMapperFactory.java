2a7a9e10d34054255f448019c466c02b12e9e1f0
Ann <265107@niuitmo.ru>
2019-10-31 21:37:50 +0300

    try1

--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,73 @@
 package com.efimchick.ifmo.web.jdbc;
 
-import java.util.Set;
-
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+        SetMapper<Set<Employee>> setMapper = resultSet -> {
+            Set<Employee> employeeSet = new HashSet<>();
+            try{
+                while(resultSet.next() == true){
+                    Employee employee = mapRow(resultSet);
+                    employeeSet.add(employee);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return employeeSet;
+        };
+        return setMapper;
+    }
+
+    private Employee mapRow(ResultSet resultSet) {
+        try {
+            BigInteger id = new BigInteger(String.valueOf(resultSet.getInt("id")));
+            FullName fullName = new FullName(
+                    resultSet.getString("firstName"),
+                    resultSet.getString("lastName"),
+                    resultSet.getString("middleName"));
+
+            Position position = Position.valueOf(resultSet.getString("position"));
+            LocalDate localDate = LocalDate.parse(resultSet.getString("hiredate"));
+            BigDecimal bigDecimal = new BigDecimal(String.valueOf(resultSet.getInt("salary")));
+            Employee manager = getManager(resultSet);
+            return new Employee(id, fullName, position, localDate, bigDecimal, manager);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    private Employee getManager(ResultSet resultSet) throws SQLException {
+        Employee manager = null;
+        if (resultSet.getObject("manager") == null)
+            return null;
+        else {
+            int cur = resultSet.getRow();
+            int idManager = resultSet.getInt("manager");
+            resultSet.absolute(0);
+            while (resultSet.next() == true) {
+                if (idManager == resultSet.getInt("id")) {
+                    manager = mapRow(resultSet);
+                    break;
+                }
+            }
+            resultSet.absolute(cur);
+        }
+        return manager;
     }
 }
+
+
+

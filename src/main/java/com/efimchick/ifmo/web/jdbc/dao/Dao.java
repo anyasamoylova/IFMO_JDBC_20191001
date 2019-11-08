b3925580e08f86e6f480cf04594cbb3fc2caebb8
Ann <265107@niuitmo.ru>
2019-11-08 21:53:34 +0300

    second

--- src/main/java/com/efimchick/ifmo/web/jdbc/dao/Dao.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/dao/Dao.java
@@ -1,16 +1,17 @@
 package com.efimchick.ifmo.web.jdbc.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface Dao<T, Id> {
 
-    Optional<T> getById(Id Id);
+    Optional<T> getById(Id Id) throws SQLException;
 
-    List<T> getAll();
+    List<T> getAll() throws SQLException;
 
-    T save(T t);
+    T save(T t) throws SQLException;
 
-    void delete(T t);
+    void delete(T t) throws SQLException;
 }
 

4603ba086d956a94bc3c3af36eb43580852f9a29
Ann <265107@niuitmo.ru>
2019-11-12 18:00:04 +0300

    try1

---
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/DaoFactory.java
@@ -0,0 +1,112 @@
+package com.efimchick.ifmo.web.jdbc.service;
+
+import com.efimchick.ifmo.web.jdbc.ConnectionSource;
+import com.efimchick.ifmo.web.jdbc.domain.Department;
+import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DaoFactory {
+
+            public static List<Employee> getEmpList(ResultSet resultSet) throws SQLException{
+                List<Employee> employees = new LinkedList<Employee>();
+                while(resultSet.next()){
+                    Employee employee = getEmployee(resultSet, false);
+                    employees.add(employee);
+                }
+                return employees;
+            }
+
+            public static Employee getEmployee(ResultSet resultSet, boolean needFull) throws SQLException {
+                BigInteger id = new BigInteger(String.valueOf(resultSet.getInt("id")));
+                FullName fullName = new FullName(
+                        resultSet.getString("firstName"),
+                        resultSet.getString("lastName"),
+                        resultSet.getString("middleName"));
+
+                Position position = Position.valueOf(resultSet.getString("position"));
+                LocalDate localDate = LocalDate.parse(resultSet.getString("hiredate"));
+                BigDecimal salary = new BigDecimal(String.valueOf(resultSet.getInt("salary")));
+                BigInteger departmentId = new BigInteger(String.valueOf(resultSet.getInt("department")));
+                Employee manager = getManager(resultSet, needFull);
+                Department department = getDepartmentById(departmentId);
+
+                return new Employee(id, fullName,position,localDate,salary,manager,department);
+            }
+
+            public static Department getDepartmentById(BigInteger Id) throws SQLException {
+                ResultSet resultSet = getResultSet("select * from DEPARTMENT");
+                List<Department> departments = getDepList(resultSet);
+                Department answer = null;
+                resultSet.close();
+                for (Department i : departments) {
+                    if (i.getId().equals(Id)) {
+                        answer = i;
+                        break;
+                    }
+                }
+                return answer;
+            }
+
+            private static List<Department> getDepList(ResultSet resultSet) throws SQLException {
+                List<Department> departments = new LinkedList<Department>();
+                while(resultSet.next()){
+                    BigInteger id = BigInteger.valueOf(resultSet.getInt("id"));
+                    String name = resultSet.getString("name");
+                    String location = resultSet.getString("location");
+                    Department department = new Department(id, name, location);
+                    departments.add(department);
+                    }
+                return departments;
+            }
+
+            private static Employee getManager(ResultSet resultSet, boolean needFull) throws SQLException {
+                ResultSet resultSet1 = getResultSet("select * from EMPLOYEE");
+                Employee manager = null;
+                int cur = resultSet.getRow();
+                if (resultSet.getObject("manager") != null) {
+                    int managerId = resultSet.getInt("manager");
+                    resultSet1.absolute(0);
+                    while(resultSet1.next()){
+                        if(managerId == resultSet1.getInt("id"))
+                            break;
+                    }
+                    if(!resultSet1.isAfterLast()){
+                        if(!needFull){
+                            manager = new Employee(
+                                    new BigInteger(String.valueOf(resultSet1.getInt("id"))),
+                                    new FullName(
+                                            resultSet1.getString("firstName"),
+                                            resultSet1.getString("lastName"),
+                                            resultSet1.getString("middleName")),
+                                    Position.valueOf(resultSet1.getString("position")),
+                                    LocalDate.parse(resultSet1.getString("hireDate")),
+                                    new BigDecimal(resultSet1.getInt("salary")),
+                                    null,
+                                    getDepartmentById(new BigInteger(String.valueOf(resultSet1.getInt("department"))))
+                            );
+                        } else {
+                            resultSet.absolute(1);
+                            while(resultSet1.getInt("id") != resultSet.getInt("id"))
+                                resultSet.next();
+                            manager = getEmployee(resultSet, true);
+                        }
+                    }
+                }
+                resultSet.absolute(cur);
+                resultSet1.close();
+                return manager;
+            }
+
+            public static ResultSet getResultSet(String str) throws SQLException {
+                    return ConnectionSource.instance().createConnection().createStatement(1004, 1008).executeQuery(str);
+            }
+}
--- src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
@@ -1,8 +1,160 @@
 package com.efimchick.ifmo.web.jdbc.service;
 
+import com.efimchick.ifmo.web.jdbc.domain.Department;
+import com.efimchick.ifmo.web.jdbc.domain.Employee;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class ServiceFactory {
 
     public EmployeeService employeeService(){
-        throw new UnsupportedOperationException();
+        EmployeeService employeeService = new EmployeeService() {
+            @Override
+            public List<Employee> getAllSortByHireDate(Paging paging) {
+                try{
+                    String str ="select * from EMPLOYEE order by hiredate";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getAllSortByLastname(Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE order by lastname";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getAllSortBySalary(Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE order by salary";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getAllSortByDepartmentNameAndLastname(Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE order by department,lastname";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByDepartmentSortByHireDate(Department department, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where department = " +
+                            department.getId()+ " order by hiredate";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByDepartmentSortBySalary(Department department, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where department = " +
+                            department.getId()+ " order by salary";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByDepartmentSortByLastname(Department department, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where department = " +
+                            department.getId()+ " order by lastname";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByManagerSortByLastname(Employee manager, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where manager = " +
+                            manager.getId()+ " order by lastname";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByManagerSortByHireDate(Employee manager, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where manager = " +
+                            manager.getId()+ " order by hiredate";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public List<Employee> getByManagerSortBySalary(Employee manager, Paging paging) {
+                try{
+                    String str = "select * from EMPLOYEE where manager = " +
+                            manager.getId()+ " order by salary";
+                    return getAnswer(str, paging);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public Employee getWithDepartmentAndFullManagerChain(Employee employee) {
+                try{
+                    ResultSet resultSet = DaoFactory.getResultSet("select * from EMPLOYEE");
+                    resultSet.absolute(0);
+                    while(resultSet.next()){
+                        if(employee.getId().intValue() == resultSet.getInt("id"))
+                            break;
+                    }
+                    Employee answer = DaoFactory.getEmployee(resultSet, true);
+                    resultSet.close();
+                    return answer;
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+
+            @Override
+            public Employee getTopNthBySalaryByDepartment(int salaryRank, Department department) {
+                try{
+                    ResultSet resultSet = DaoFactory.getResultSet("select * from EMPLOYEE where department = " +
+                            department.getId() + " order by salary desc");
+                    List<Employee> answer = DaoFactory.getEmpList(resultSet);
+                    resultSet.close();
+                    return answer.get(salaryRank - 1);
+                } catch (SQLException e){
+                    return null;
+                }
+            }
+        };
+        return employeeService;
+    }
+    private List<Employee> getAnswer(String str, Paging paging) throws SQLException {
+        ResultSet resultSet = DaoFactory.getResultSet(str);
+        List<Employee> answer = DaoFactory.getEmpList(resultSet);
+        resultSet.close();
+        int min = paging.itemPerPage * (paging.page - 1);
+        int max = paging.itemPerPage * paging.page;
+        return answer.subList(min, (max > answer.size() ? answer.size(): max));
     }
 }

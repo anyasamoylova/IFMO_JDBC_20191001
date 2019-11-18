7d991f0972e0b679b8640729e19b6af23ee2758c
thejerome <thejerome@mail.ru>
2019-11-18 15:52:12 +0300

    Update DaoFactory.java

--- src/main/java/com/efimchick/ifmo/web/jdbc/service/DaoFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/DaoFactory.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class DaoFactory {
 
             public static List<Employee> getEmpList(ResultSet resultSet) throws SQLException{
